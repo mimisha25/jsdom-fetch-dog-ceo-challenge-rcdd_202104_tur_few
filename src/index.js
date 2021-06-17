29327066316762b829d192a6bbe24209361ff44e
Aziza  iliasova <miacorporation25@gmail.com>
2021-06-17 22:38:10 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -10,15 +10,12 @@ console.log('%c HI', 'color: firebrick');
      image.setAttribute("src",json.message[i]);
      let imgDiv = document.querySelector("#dog-image-container");
      imgDiv.append(image);
-
      }
-
    });
 
    fetch(breedUrl).then(result => {return result.json()})
     .then(json =>{
        for(let i=0;i<json.message.length;i++){
-
          let breed = document.createElement("li");
          breed.innerHTML = json.message[i];
          breed.addEventListener("click",()=>{
@@ -27,23 +24,16 @@ console.log('%c HI', 'color: firebrick');
          let breedList = document.querySelector("#dog-breeds");
          breedList.append(breed);
 
-
          let selector=document.querySelector("#breed-dropdown");
          selector.addEventListener("change",function(){
            for(let i=0;i<json.message.length;i++){
-
             if(breed.innerHTML.startsWith(selector.value) ){
               breed.style.display = "block";
             }else{
               breed.style.display = "none";
             }
-
            }
-
          });
        }
-
-
     });
-
  });
